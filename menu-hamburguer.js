8581358a38947e874443a641733b8143189ac92c
Débora França <framesframes562@gmail.com>
2026-07-23 22:15:51 -0300

    Aplicando CSS e JS no menu header

--- menu-hamburguer.js
+++ menu-hamburguer.js
@@ -4,10 +4,32 @@
 // Fechar o menu
 
 const btnMenu = document.getElementById("btn-menu")
+const navPrincipal = document.getElementById("navegacao-primaria")
+const icone = btnMenu.querySelector("i")
+
+console.log(navPrincipal)
 
 // addEventListener = Serve para observar ações/eventos em elementos do HTML. E executar uma função quando esse evento acontece
-btnMenu.addEventListener("click", abriMenu)
+btnMenu.addEventListener("click", abrirMenu)
 
-function abriMenu() {
-    console.log("Clicou no menu")
+function abrirMenu() {
+//   navPrincipal.style.display = "block"
+
+navPrincipal.classList.toggle("nav-ativa")
+
+
+// Verifica se o menu está aberto. O método contains, retorna verdadeiro ou falso
+const menuAberto = navPrincipal.
+classList.contains("nav-ativa")
+
+// Atualiza acessibilidade
+btnMenu.setAttribute("aria-expanded", menuAberto)
+
+if(menuAberto){
+    icone.classList.remove("bi-list")
+    icone.classList.add("bi-x")
+} else {
+   icone.classList.remove("bi-x")
+    icone.classList.add("bi-list")  
+}
 }
